b2285cb62a65b76ac9ec9f04f436fb95596771d6
Mattyelele <65793571+Mattyelele@users.noreply.github.com>
2021-05-27 11:22:17 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = 'A';
+            int number = 100;
+            float bodyTemp = 98.6f;
+            double pi = 3.14159;
+            decimal largeNumber = 1000.00m;
+            bool flag = false;
+            string text = "I love C#";
+
+            Console.WriteLine("Char letter: \t" + letter);
+            Console.WriteLine("int number: \t" + number);
+            Console.WriteLine("float body temp: \t" + bodyTemp);
+            Console.WriteLine("double pi: \t" + pi);
+            Console.WriteLine("decimal sum: \t" + largeNumber);
+            Console.WriteLine("bool flag \t" + flag);
+            Console.WriteLine("string text: \t" + text);
+            Console.ReadKey();
+        }
+    }
+}
